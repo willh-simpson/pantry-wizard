6dff88220fbd1ce066dda1579f41d8678b0436e8
Will Simpson <willh.simpson01@gmail.com>
2026-03-27 22:57:16 -0400

    Added checks for recipe matches and adding items from web results to wishlist/pantry.

--- mobile/app/src/main/java/com/wills/pantrywizard/presentation/recipedetail/RecipeDetailViewModel.kt
+++ mobile/app/src/main/java/com/wills/pantrywizard/presentation/recipedetail/RecipeDetailViewModel.kt
@@ -1,13 +1,14 @@
 package com.wills.pantrywizard.presentation.recipedetail
 
-import androidx.compose.animation.core.animateIntSizeAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.wills.pantrywizard.domain.model.IngredientMatch
 import com.wills.pantrywizard.domain.repository.AiRepository
+import com.wills.pantrywizard.domain.usecase.CheckIngredientsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -15,11 +16,14 @@ import javax.inject.Inject
 @HiltViewModel
 class RecipeDetailViewModel @Inject constructor(
     private val aiRepository: AiRepository,
+    private val checkIngredientsUseCase: CheckIngredientsUseCase,
     savedStateHandle: SavedStateHandle // grabs URL passed from previous screen
 ) : ViewModel() {
     private val recipeUrl: String = checkNotNull(savedStateHandle["recipeUrl"])
 
     var uiState by mutableStateOf(RecipeDetailUiState())
+        private set
+    var ingredientMatches by mutableStateOf<List<IngredientMatch>>(emptyList())
         private set
 
     init {
@@ -49,10 +53,20 @@ class RecipeDetailViewModel @Inject constructor(
             aiRepository.extractFullRecipe(recipeUrl)
                 .onSuccess { detail ->
                     uiState = uiState.copy(recipe = detail, isLoading = false)
+
+                    checkIngredients(detail.ingredients) // ingredient matches should be checked immediately
                 }
                 .onFailure { exception ->
                     uiState = uiState.copy(error = exception.message, isLoading = false)
                 }
+        }
+    }
+
+    private fun checkIngredients(ingredients: List<String>) {
+        viewModelScope.launch {
+            checkIngredientsUseCase(ingredients).onSuccess { matches ->
+                ingredientMatches = matches
+            }
         }
     }
 }
